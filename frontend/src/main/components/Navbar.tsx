b6c1a28a7844f125cb12d0b85884cd549bbd6ce3
Henry Ly <lyhenry2004@gmail.com>
2026-05-18 22:54:11 +1200

    Styled the navbar to match figma

--- frontend/src/main/components/Navbar.tsx
+++ frontend/src/main/components/Navbar.tsx
@@ -2,11 +2,19 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div className="w-full h-16 bg-[#0F6E56] flex flex-row items-center justify-between px-4">
+    <div className="w-full h-16 bg-[#0F6E56] flex flex-row items-center justify-between px-8">
       <h1>My App</h1>
-      <div className="flex space-x-4">
-        <Link to="/modules">Learn</Link>
-        <Link to="/about">About</Link>
+      <div className="flex space-x-16">
+        <Link to="/modules">
+          <h2 className="hover:opacity-70">
+            Learn
+          </h2>
+        </Link>
+        <Link to="/about">
+          <h2 className="hover:opacity-70">
+            About
+          </h2>
+        </Link>
       </div>
     </div>
   )
