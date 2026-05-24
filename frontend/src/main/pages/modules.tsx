7ef4390b42e98ee1795fb001eec29b4a32245965
Alex Tee <atee059@aucklanduni.ac.nz>
2026-05-24 18:09:47 +1200

    Implemented full story, integrated with activities

--- frontend/src/main/pages/modules.tsx
+++ frontend/src/main/pages/modules.tsx
@@ -9,8 +9,8 @@ const Modules = () => {
       <div className="flex flex-col px-40 grid-lessons my-32 gap-8">
 				<ModuleCardFull
 					moduleNum={1}
-					title="Interacting with people online" 
-					description="Help Tāne and Ruru learn how to interact with other people online!"
+					title="Scams and Phishing" 
+					description="Kōro, Ruru and Āroha learn how to identify and avoid scams and phishing attempts!"
 					imgUrl="/card.png"
 					linkUrl="/vn"
 				/> 
@@ -18,15 +18,15 @@ const Modules = () => {
 				<ModuleCardFull
 					moduleNum={2}
 					title="Viruses & Malware" 
-					description="Tāne and Ruru explore what happens if you get infected with a virus!"
+					description="Tāne and Āroha explore what happens if you get infected with a virus!"
 					imgUrl="/card.png"
 					linkUrl="/modules"
 				/> 
 
 				<ModuleCardFull 
 					moduleNum={3}
-					title="Scammers" 
-					description="Tāne gets a message offering him free robux!"
+					title="Online Privacy" 
+					description="Ruru and Āroha learn about online privacy and how to protect their personal information!"
 					imgUrl="/card.png"
 					linkUrl="/modules"
 				/> 
@@ -34,7 +34,7 @@ const Modules = () => {
 				<ModuleCardFull 
 					moduleNum={4}
 					title="Cyberbullying" 
-					description="Someone is messaging Tāne mean messages! Can Ruru help?"
+					description="Someone is messaging Tāne mean messages! Can Kōro help?"
 					imgUrl="/card.png"
 					linkUrl="/modules"
 				/> 
