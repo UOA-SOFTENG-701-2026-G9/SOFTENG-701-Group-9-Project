d9e790598bb4429c3d86291b8b3a6d948fedcc00
Thomas Dickson <git@hoverth.net>
2026-05-24 12:22:13 +1200

    feat: add modules page

---
+++ frontend/src/main/components/home/ModuleCardFull.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const ModuleCardFull = ({title, description, imgUrl, linkUrl}) => {
+  return (
+   <div class="px-4 max-w-1/2 min-w-80"> 
+        <article class="mb-4 bg-gray-800 rounded overflow-hidden shadow-lg"> 
+          <img class="w-full" src={imgUrl}/>  
+          <div class="px-9 py-4 flex flex-col items-center"> 
+            <h2>{title}</h2> 
+            <p className="text-gray-200 py-2 text-base">{description}</p> 
+ 
+            <Link to={linkUrl} class="min-w-8 max-w-23 py-1 px-3 bg-[#EF9F27] rounded-full hover:bg-[#D48A1A] transition-colors duration-200 items-center">  
+              <h3> Check it out </h3> 
+            </Link> 
+          </div> 
+        </article> 
+      </div>
+  )
+}
+
+export default ModuleCardFull
--- frontend/src/main/pages/modules.tsx
+++ frontend/src/main/pages/modules.tsx
@@ -1,8 +1,47 @@
+import ModuleCardFull from "../components/home/ModuleCardFull";
+
 const Modules = () => {
+
   return (
     <div>
-      <h1>Modules Page</h1>
-      <p>Welcome to the modules page!</p>
+      <header class="px-10 mb-4">
+        <h1>Modules</h1>
+        <p>Welcome to the modules page!</p>
+	<p class="py-2">Pick a topic to start learning!</p>
+      </header>
+
+      <div class="flex flex-wrap px-10 -mb-4 grid-lessons">
+        <ModuleCardFull 
+	  title="Interacting with people online" 
+	  description="Help Tāne and Ruru learn how to interact with other people online!"
+	  imgUrl="/public/card.png"
+	  linkUrl="/modules"
+	/> 
+          
+	<ModuleCardFull 
+	  title="Viruses & Malware" 
+	  description="Tāne and Ruru explore what happens if you get infected with a virus!"
+	  imgUrl="/public/card.png"
+	  linkUrl="/modules"
+	/> 
+
+	<ModuleCardFull 
+	  title="Scammers" 
+	  description="Tāne gets a message offering him free robux!"
+	  imgUrl="/public/card.png"
+	  linkUrl="/modules"
+	/> 
+
+	<ModuleCardFull 
+	  title="Cyberbullying" 
+	  description="Someone is messaging Tāne mean messages! Can Ruru help?"
+	  imgUrl="/public/card.png"
+	  linkUrl="/modules"
+	/> 
+
+      </div>
+
+      <div class="px-4 py-4"></div>
     </div>
   )
 }
